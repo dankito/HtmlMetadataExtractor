be732f059fe5115e6a68ea4893e21a4a6ef6b742
dankito <git@dankito.net>
2026-06-05 00:53:37 +0200

    Added scripts to publish library

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 group = "net.dankito.web"
 version = "1.0.0-SNAPSHOT"
 
+ext["customArtifactId"] = "html-metadata-extractor"
+
+ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/HtmlMetadataExtractor"
+
+ext["projectDescription"] = "Extracts web page metadata from HTML"
+
+
 
 kotlin {
     jvmToolchain(11)
@@ -26,4 +33,9 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+
+if (file("./gradle/scripts/publish-dankito.gradle.kts").exists()) {
+    apply(from = "./gradle/scripts/publish-dankito.gradle.kts")
 }
